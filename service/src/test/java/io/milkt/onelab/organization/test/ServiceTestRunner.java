57abd1ef7e5dd4a6cdb5ac6976262d9f209483be
Michael.Lee <leewind19841209@gmail.com>
2017-11-14 07:56:38 +0800

    [dev]提交完成代码

---
+++ service/src/test/java/io/milkt/onelab/organization/test/ServiceTestRunner.java
@@ -0,0 +1,10 @@
+package io.milkt.onelab.organization.test;
+
+/**
+ * Created by leewind on 2017/8/21.
+ */
+public class ServiceTestRunner {
+    public static void main(String[] args) throws Exception {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
